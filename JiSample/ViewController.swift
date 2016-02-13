37701af5a402bd1aac9446ce9e4606c0a33ef083
SUZUKIyuka <suzukiyuka1218@gmail.com>
2016-02-13 16:16:00 +0900

    Initial Commit

---
+++ JiSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JiSample
+//
+//  Created by nagata on 2/13/16.
+//  Copyright © 2016 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
